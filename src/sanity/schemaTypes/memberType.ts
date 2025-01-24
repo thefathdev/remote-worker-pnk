6c8be871aaca9b2d0b4776700b386ca321f44d7e
Fath Irfaan <abdillahfathu@gmail.com>
2025-01-24 15:01:08 +0700

    Enhance member schema with icons and preview configuration for improved content management

--- src/sanity/schemaTypes/memberType.ts
+++ src/sanity/schemaTypes/memberType.ts
@@ -1,26 +1,31 @@
 import { defineField, defineType } from "sanity";
+import { PiCardholder, PiImage, PiLaptop, PiUserBold } from "react-icons/pi";
 
 export const memberType = defineType({
   name: "members",
   title: "Members",
   type: "document",
+  icon: PiUserBold,
   fields: [
     defineField({
       name: "name",
       type: "string",
       validation: (rule) => rule.required(),
       title: "Member Name",
+      icon: PiCardholder,
     }),
     defineField({
       name: "title",
       type: "string",
       validation: (rule) => rule.required(),
       title: "Member Title",
+      icon: PiLaptop,
     }),
     defineField({
       name: "photo",
       type: "image",
       title: "Member Photo",
+      icon: PiImage,
     }),
     defineField({
       name: "slug",
@@ -29,4 +34,11 @@ export const memberType = defineType({
       validation: (rule) => rule.required(),
     }),
   ],
+
+  preview: {
+    select: {
+      title: "name",
+      subtitle: "title",
+    },
+  },
 });
