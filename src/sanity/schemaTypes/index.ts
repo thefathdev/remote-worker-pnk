5dc998f38adff4e70bc43129a42f11edd8c88dcc
Fath Irfaan <abdillahfathu@gmail.com>
2025-06-01 05:39:26 +0700

    Add new schema types for author, block content, category, and post in Sanity, and update index to include these types

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,6 +1,10 @@
 import { type SchemaTypeDefinition } from "sanity";
 import { memberType } from "./memberType";
+import { blockContentType } from "./blockContentType";
+import { authorType } from "./authorType";
+import { postType } from "./postType";
+import { categoryType } from "./categoryType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [memberType],
+  types: [memberType, blockContentType, authorType, postType, categoryType],
 };
